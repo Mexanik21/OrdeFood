52ba78efbe07bcde30d3811813abaae533edfb87
Abdullaziz <Abdullaziz0712@gmail.com>
2022-08-26 17:40:39 +0500

    Initial commit

---
+++ src/main/kotlin/uz/minfin/ordefood/OrdeFoodApplication.kt
@@ -0,0 +1,11 @@
+package uz.minfin.ordefood
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrdeFoodApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrdeFoodApplication>(*args)
+}
